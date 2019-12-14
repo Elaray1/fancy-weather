eda97de3858f81d847bd06477e18948a918b8d69
Egor <5642452@mail.ru>
2019-12-14 19:18:16 +0300

    19

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,7 @@ import { weatherFor3DaysBlock } from './weatherFor3Days';
 import { mapBlock } from './map';
 import { weatherArrayEng, weatherArrayRu, weatherArrayBe } from './weatherArrays';
 
+const microfonImg = document.getElementById('microfon-img');
 window.onload = async function () {
   let language;
   if (localStorage.getItem('language') !== null) {
@@ -37,7 +38,6 @@ window.onload = async function () {
   const searchInput = document.getElementById('search-input');
   const searchBtn = document.getElementById('search-btn');
   const languageBlock = document.querySelector('select');
-  const microfonImg = document.getElementById('microfon-img');
   let [lng, lat] = await getCoordinates(await getUserCity());
 
   languageBlock.value = language;
