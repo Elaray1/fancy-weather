c44ee1a7bdc08bbf48bfeea73718dfca5a92996d
Egor <5642452@mail.ru>
2019-12-14 18:43:39 +0300

    5

--- src/js/speechRecognition.js
+++ src/js/speechRecognition.js
@@ -1,19 +1,21 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-const searchInput = document.getElementById('search-input');
+window.onload = () => {
+  const searchInput = document.getElementById('search-input');
 
-const recognition = new SpeechRecognition();
-recognition.interimResults = true;
+  const recognition = new SpeechRecognition();
+  recognition.interimResults = true;
 
-recognition.start();
+  recognition.start();
 
-recognition.addEventListener('result', (e) => {
-  const transcript = Array.from(e.results)
-    .map((result) => result[0])
-    .map((result) => result.transcript)
-    .join('');
+  recognition.addEventListener('result', (e) => {
+    const transcript = Array.from(e.results)
+      .map((result) => result[0])
+      .map((result) => result.transcript)
+      .join('');
 
-  searchInput.value = transcript;
-});
+    searchInput.value = transcript;
+  });
 
-recognition.addEventListener('end', recognition.start);
+  recognition.addEventListener('end', recognition.start);
+};
