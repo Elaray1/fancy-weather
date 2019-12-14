328c482110bc31853cd342a200e403415ffabf6c
Egor <5642452@mail.ru>
2019-12-14 18:41:51 +0300

    4

--- src/js/speechRecognition.js
+++ src/js/speechRecognition.js
@@ -1,6 +1,6 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-const searchInput = document.getElementById('seatch-input');
+const searchInput = document.getElementById('search-input');
 
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
@@ -11,9 +11,9 @@ recognition.addEventListener('result', (e) => {
   const transcript = Array.from(e.results)
     .map((result) => result[0])
     .map((result) => result.transcript)
-    .join('')
+    .join('');
 
   searchInput.value = transcript;
 });
 
-recognition.addEventListener('end', recognition.start)
+recognition.addEventListener('end', recognition.start);
